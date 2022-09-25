51e76fb0be29f535f5cbef09847911963c8e73a4
ms-rawat <moharsinghzxz@gmail.com>
2022-09-25 12:15:27 +0530

    inorder traversal

---
+++ Treetraversal.java
@@ -0,0 +1,40 @@
+class Node{
+    String name;
+    Node left,right;
+    public Node(String NAME)
+    {
+        name = NAME;
+        left=right=null;
+
+    }
+}
+public class Treetraversal{
+   Node root;
+ Treetraversal(){ root = null;}
+ void printInOrder(Node node)
+ {
+    if(node == null)
+    return;
+    printInOrder(node.left);
+    System.out.println(node.name+" ");
+    printInOrder(node.right);
+ }
+//  void printInOrder(){
+//     printInOrder(root);
+//  }
+    public static void main(String[] args) {
+        Treetraversal tree = new Treetraversal();
+        tree.root=new Node("mohar");
+        tree.root.left=new Node("chintu");
+        tree.root.right=new Node("pintu");
+        tree.printInOrder(tree.root);
+
+        
+    }
+
+
+
+
+
+
+}
